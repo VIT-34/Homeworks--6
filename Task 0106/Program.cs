46a9027957c42ba12c4fe81d852595dd90b13d74
Vitaliy <viurum986@mail.ru>
2022-03-29 18:20:19 +0300

    init task 0106

---
+++ Task 0106/Program.cs
@@ -0,0 +1,22 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+int a = Prompt("Введите число, которое определит максимальное количество чисел вводимых пользователем => ");
+
+int count = 0;
+
+for(int i = 0; i < a; i++)
+{
+Console.Write($"Введите число {i+1}: ");
+int b = int.Parse(Console.ReadLine());
+if(b > 0)
+count++;
+}
+Console.Write($"количество чисел больше 0: {count}");
