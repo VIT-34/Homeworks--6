ace95f4afa1ca820631d3d83e6eb7701516733c8
Vitaliy <viurum986@mail.ru>
2022-03-30 20:38:54 +0300

    init task 0206

--- Task 0206/Program.cs
+++ Task 0206/Program.cs
@@ -14,9 +14,7 @@ double k1 = Promt("Введите значение k1 => ");
 double b2 = Promt("Введите значение b2 => ");
 double k2 = Promt("Введите значение k2 => ");
 
-double x = (b2 - b1) / (k2 - k1);
-double y1 = (k1 * x + b1);
-double y2 = (k2 * x + b2);
+double x = (b2 - b1) / (k1 - k2);
+double y = (k1 * x + b1);
 
-Console.WriteLine(( x, k1 * x + b1 ));
-Console.WriteLine((x, k2 * x + b2));
+Console.WriteLine(( x, y));
