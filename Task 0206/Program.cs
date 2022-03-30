ccbb2cb1f628df315d1ec43fc90cd7ad5f0e63a7
Vitaliy <viurum986@mail.ru>
2022-03-30 17:20:34 +0300

    init task 0206

--- Task 0206/Program.cs
+++ Task 0206/Program.cs
@@ -1,12 +1,22 @@
 ﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 *x + b1, y = k2 * x + b2;
 // значения b1, k1, b2 и k2 задаются пользователем.
 
-int Prompt(string message)
+double Promt(string message)
 {
     Console.Write(message);
-    string strInput = Console.ReadLine();
-    int answer = int.Parse(strInput);
-    return answer;
+    string strValue = Console.ReadLine();
+    double Value = double.Parse(strValue);
+    return Value;
 }
 
-int a = Prompt("Введите число, которое определит максимальное количество чисел вводимых пользователем => ");
+double b1 = Promt("Введите значение b1 => ");
+double k1 = Promt("Введите значение k1 => ");
+double b2 = Promt("Введите значение b2 => ");
+double k2 = Promt("Введите значение k2 => ");
+
+double x = (b2 - b1) / (k2 - k1);
+double y1 = (k1 * x + b1);
+double y2 = (k2 * x + b2);
+
+Console.WriteLine(( x, k1 * x + b1 ));
+Console.WriteLine((x, k2 * x + b2));
